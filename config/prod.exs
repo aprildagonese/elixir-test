ee934dd2091d3af147671b7f9c817bbb9b5a3932
April Dagonese <adagonese@gmail.com>
2019-05-18 16:32:47 -0600

    heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -17,21 +17,24 @@ config :elixir_test, ElixirTestWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :elixir_test, ElixirTestWeb.Endpoint,
+  load_from_system_env: true,
+  url: [host: "tranquil-dawn-86335.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :elixir_test, ElixirTest.Repo,
-adapter: Ecto.Adapters.Postgres,
-url: System.get_env("DATABASE_URL"),
-pool_size: String.to_integer(System.get_env("POOL_SIZE") || "18"),
-ssl: false
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-      config :elixir_test, ElixirTestWeb.Endpoint,
-        url: [scheme: "http", host: "tranquil-dawn-86335.herokuapp.com", port: 443],
-        force_ssl: [rewrite_on: [:x_forwarded_proto]],
 #
 # The `cipher_suite` is set to `:strong` to support only the
 # latest and more secure SSL ciphers. This means old browsers
