3713a84ed832e6d88b1d5f23de3f7e0d2da8a355
April Dagonese <adagonese@gmail.com>
2019-05-18 12:50:44 -0600

    Adds .env file

--- config/travis.exs
+++ config/travis.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :elixir_test, ElixirTest.Endpoint,
-  http: [port: 5432],
+  http: [port: 4001],
   server: true
 
 # Print only warnings and errors during test
