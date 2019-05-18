cc29f08cf0134e371f49dbe5bdd333a665c79b60
April Dagonese <adagonese@gmail.com>
2019-05-18 10:18:39 -0600

    config updates

--- config/travis.exs
+++ config/travis.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :elixir, ElixirTest.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "",
+  password: "postgres",
   database: "elixir_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
